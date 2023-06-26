0a9d18ff538b0609c62b76e2b778daff3bd8a1e3
euntaek419 <cet4713@gmail.com>
2023-06-26 22:17:58 +0900

    2023/06/26_초기설정

--- src/App.vue
+++ src/App.vue
@@ -20,33 +20,20 @@
   <div>
     10000원 구매시 필요한 금액 : {{ Math.abs(((salecash * 0.01 + 1) * 10000) - 10000 - 10000) }} 원
   </div>
-
-  <div>
-    Type a number: <input v-model.number="number" />
-    <p>{{ tweened.toFixed(0) }}</p>
-  </div>
   
 </template>
 
 <script>
-import gsap from 'gsap'
 
 export default {
   data: () => {
     return {
       salecash : '',
-      number: 0,
-      tweened: 0,
     }
   },
   name: 'App',
   components: {
   },
-  watch: {
-    number(n) {
-      gsap.to(this, { duration: 0.5, tweened: Number(n) || 0 })
-    }
-  }
 }
 </script>
 
