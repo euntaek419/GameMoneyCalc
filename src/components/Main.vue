15cda2ebd1fa269e3fd5c82beca526a10575089f
euntaek419 <cet4713@gmail.com>
2023-06-28 13:41:04 +0900

    2023/06/28_로고 제작 및 컴포넌트 분할

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,26 @@
 <template>
-  <div></div>
+    <div>게임 머니 계산기</div>
+  
+  <div>
+    <label> 캐시 할인 : </label>
+    <label> 캐시 <select> <option> 할인</option> <option> 업</option></select> : </label>
+    <input placeholder="0" v-model="salecash"> % (문화상품권, 넥슨카드 등)
+  </div>
+
+  <div>
+    <label>게임 머니 가격 : </label>
+    <input placeholder="100"> : 1 백만 비율 (원(N) : 게임 머니(1))
+  </div>
+
+  <div>
+    <label>구매 아이템 가격 : </label>
+    <input placeholder="원"> <input placeholder="게임머니">
+  </div>
+
+  <div>
+    10000원 구매시 필요한 금액 : {{ Math.abs(((salecash * 0.01 + 1) * 10000) - 10000 - 10000) }} 원
+  </div>
+  
 </template>
 
 <script>
---
+++ src/components/Title.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="Title_img">
+      <img src="../assets/images/temp.png">
+    </div>
+
+    <div>
+      <div class="Title_name">GAMEMONEY<b class="Gray">CALC</b></div>
+    </div>
+
+    <div>
+      <div class="Subtitle">게임머니계산기</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.Title_img{
+  position: fixed;
+  top: 15%;
+  left: 10%;
+  /* cursor: pointer; */
+  /* z-index: 1; */
+}
+
+.Title_name{
+  position: fixed;
+  top: 30%;
+  left: 10%;
+
+  color:#fff;
+  font-size: 220px;
+  font-family: "MorganiteBold";
+  background-color: transparent;
+}
+
+.Gray{
+  color: #9b9b9b;
+  background-color: transparent;
+}
+
+.Subtitle{
+  position: fixed;
+  top: 52%;
+  left: 10%;
+  
+  font-family: "NeurimboGothicRegular";
+  color : #fff;
+  font-size: 55px;
+  background-color: transparent;
+}
+
+</style>
